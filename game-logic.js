83895e79f730e00520e1f5b934c6c02fef1bcc0e
joshua-wilson2396 <joshuawilson2396@outlook.com>
2019-12-05 12:08:13 -0500

    basic intro down abstracted logic, player instance

--- game-logic.js
+++ game-logic.js
@@ -1,4 +1,11 @@
 'use strict'
 
-let hello = document.getElementById('hello')
-hello.textContent = 'Hello World'
+let hello = document.getElementById('hey-youre-finally-awake')
+hello.textContent = `Hey, you're finally awake, get up, they'll be back to kill us soon.`
+
+let statRead = document.getElementById('stat-read')
+statRead.textContent = `From the reading I did on you, this is what you're working with. Your HP is ${player.hp}, your XP is ${player.xp}, and your
+mana is ${player.mana}.`
+
+let getName = document.getElementById('name')
+getName.textContent = "Well, I saved your life, least you could do is tell me your name."
